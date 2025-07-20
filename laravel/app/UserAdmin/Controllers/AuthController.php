0145554431cbcb8705fedf2c464e10e474d1fdae
dongasai <1514582970@qq.com>
2025-07-20 21:14:37 +0800

    认证修复

--- laravel/app/UserAdmin/Controllers/AuthController.php
+++ laravel/app/UserAdmin/Controllers/AuthController.php
@@ -9,29 +9,7 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends BaseAuthController
 {
-    /**
-     * 重写用户名字段，使用email进行认证
-     *
-     * @return string
-     */
-    protected function username()
-    {
-        return 'email';
-    }
+  
+    
 
-    /**
-     * 重写登录处理方法，将username字段映射为email
-     *
-     * @param Request $request
-     * @return mixed
-     */
-    public function postLogin(Request $request)
-    {
-        // 将username字段映射为email字段
-        if ($request->has('username')) {
-            $request->merge(['email' => $request->input('username')]);
-        }
-
-        return parent::postLogin($request);
-    }
 }

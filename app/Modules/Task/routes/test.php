1264fe1bc8d58ef6d2da7428b1dda04c151e5013
dongasai <1514582970@qq.com>
2025-07-27 03:10:30 +0800

    移除旧得模块

--- app/Modules/Task/routes/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Modules\Task\Controllers\TaskModelTestController;
-use App\Modules\Task\Controllers\TaskWorkflowTestController;
-
-# 任何路由的产生都是错误的，内部模块不提供API,不提供网页，测试也不允许
